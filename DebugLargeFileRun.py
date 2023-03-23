2447beec9e7a7b288c1d5a2e591afef470f453ff
Muyeed Ahmed <muyeedahmed@Muyeeds-MacBook-Pro.local>
2023-03-23 05:53:40 -0400

    OCSVM n_iter check

--- DebugLargeFileRun.py
+++ DebugLargeFileRun.py
@@ -3,11 +3,12 @@ import os
 import shutil
 import glob
 import pandas as pd
-import mat73
+# import mat73
 from scipy.io import loadmat
 from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.svm import OneClassSVM
+from sklearn.covariance import EllipticEnvelope
 import numpy as np
 from sklearn import metrics
 from copy import copy, deepcopy
@@ -18,40 +19,67 @@ import time
 from sklearn.utils import shuffle
 import csv
 from scipy.io import arff
-import threading
-# from memory_profiler import profile
-
 
 
 datasetFolderDir = '../Dataset/Data_Small/'
 
+def algoRun(filename):
+    [X, y] = readData(filename)
 
-fileName = "analcatdata_apnea1"
-df = pd.read_csv(datasetFolderDir+fileName+".csv")
+    clf = OneClassSVM().fit(X)
 
-if "target" in df.columns:
-    y=df["target"].to_numpy()
-    X=df.drop("target", axis=1)
-elif "outlier" in df.columns:
-    y=df["outlier"].to_numpy()
-    X=df.drop("outlier", axis=1)
-else:
-    print("Ground Truth not found")
-
-
-t0 = time.time()
-c = OneClassSVM(nu=.12).fit(X)
-l = c.predict(X)
-l = [0 if x == 1 else 1 for x in l]
-
-f1 = (metrics.f1_score(y, l))
-
-t1 = time.time()
-print("Default--")
-print("F1: ", f1, " and Time: ", t1-t0)
+    iter_ = clf.n_iter_
+    f=open("Stats/OCSVM_nIter.csv", "a")
+    f.write(filename+","+str(iter_)+","+str(X.shape[0])+","+str(X.shape[1])+"\n")
+    f.close()
     
     
-        
     
-        
-        
+
+def readData(fileName):
+    df = pd.read_csv(datasetFolderDir+fileName+".csv")
+    if df.shape[0] > 100000:
+        return True
+    
+    df = shuffle(df)
+    if "target" in df.columns:
+        y=df["target"].to_numpy()
+        X=df.drop("target", axis=1)
+    elif "outlier" in df.columns:
+        y=df["outlier"].to_numpy()
+        X=df.drop("outlier", axis=1)
+    else:
+        print("Ground Truth not found")
+ 
+    return X, y
+
+def plot_iter():
+    df = pd.read_csv("StatsOCSVM_nIter.csv")
+    ax1 = df.plot.scatter(x='n_iter', y='rows', c='DarkBlue')
+
+
+if __name__ == '__main__':
+    folderpath = datasetFolderDir
+    master_files = glob.glob(folderpath+"*.csv")
+    
+    for i in range(len(master_files)):
+        master_files[i] = master_files[i].split("/")[-1].split(".")[0]
+    
+    master_files.sort()
+
+    
+    if os.path.exists("Stats/OCSVM_nIter.csv") == 0:
+        f=open("Stats/OCSVM_nIter.csv", "w")
+        f.write('Filename,n_iter,rows,col\n')
+        f.close()
+    
+    
+    # for file in master_files:
+    #     print(file)
+    #     try:
+    #         algoRun(file)
+    #     except:
+    #         print("Fail")
+            
+    plot_iter()    
+            
