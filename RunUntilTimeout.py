dfe44eab2fc417cd24b3e79662e93551ce81813a
MuyeedAhmed <akib100095@gmail.com>
2023-03-28 07:05:00 -0400

    OCSVM RunUntilTimeout Done

--- RunUntilTimeout.py
+++ RunUntilTimeout.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 23 05:55:46 2023
-
-@author: muyeedahmed
-"""
-
 import sys
 import os
 import shutil
@@ -25,7 +17,7 @@ from sklearn.utils import shuffle
 import multiprocessing
 
 
-datasetFolderDir = '../Dataset/Data_Small/'
+datasetFolderDir = '../Dataset/Small/'
 # datasetFolderDir = 'Temp/'
 
 
@@ -38,7 +30,9 @@ def algoRun(filename):
     p.join(timeout=7200)
     if p.is_alive():
         p.terminate()
-    
+    else:
+        print(filename, " - Complete")
+        return
     step = "while_loop"
     if os.path.exists("Log/while_loop.txt"):
         with open('Log/while_loop.txt', 'rb') as f:
@@ -94,10 +88,15 @@ if __name__ == '__main__':
         done_files = done_files["Filename"].to_numpy()
         master_files = [x for x in master_files if x not in done_files]
     
+    if os.path.exists("Stats/"+algorithm+"_Incomplete.csv"):
+        done_files = pd.read_csv("Stats/"+algorithm+"_Incomplete.csv")
+        done_files = done_files["Filename"].to_numpy()
+        master_files = [x for x in master_files if x not in done_files]
+    
     master_files.sort()
         
-    if os.path.exists("Stats/OCSVM_Incomplete.csv") == 0:
-        f=open("Stats/OCSVM_Incomplete.csv", "w")
+    if os.path.exists("Stats/"+algorithm+"_Incomplete.csv") == 0:
+        f=open("Stats/"+algorithm+"_Incomplete.csv", "w")
         f.write('Filename,Step,n_iter,rows,col\n')
         f.close()
     
