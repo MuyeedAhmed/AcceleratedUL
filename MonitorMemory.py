bd0d3992da77094937fad17d3d0596ef00c93350
Muyeed Ahmed <muyeedahmed@Muyeeds-MacBook-Pro.local>
2023-05-20 22:35:48 -0400

    Test louise

--- MonitorMemory.py
+++ MonitorMemory.py
@@ -4,10 +4,14 @@ import time
 def monitor_memory_usage(interval):
     print("Memory usage")
     while True:
-        memory_info = psutil.virtual_memory()
-        memory_usage = memory_info.used / (1024 * 1024)  # Convert to megabytes
-        print(f"{memory_usage:.2f} MB", end=' ')
-        time.sleep(interval)
+        memory = []
+        for _ in range(10):
+            memory_info = psutil.virtual_memory()
+            memory_usage = memory_info.used / (1024 * 1024)  # Convert to megabytes
+            memory.append(memory_usage)
+            time.sleep(interval)
+        print([ '%.2f' % elem for elem in memory])
+        
 
 # Example usage
 monitor_memory_usage(interval=1)
