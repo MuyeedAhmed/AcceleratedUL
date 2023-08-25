37c0c56fd8574e886caf300d1bf682987ebb9ba9
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-08-25 02:17:31 -0400

    Test prev data on Jimmy for DBSCAN

--- AvgTimeFill.py
+++ AvgTimeFill.py
@@ -15,8 +15,8 @@ def AvgTime(algo, mode):
     
     print(algo, mode)
 
-    # files = ["har_OpenML", "pendigits_OpenML", "eye_movements_OpenML"]
-    files = ["letter_OpenML", "BNG(autos_COMMA_nominal_COMMA_1000000)_OpenML", "BNG(lymph_COMMA_nominal_COMMA_1000000)_OpenML"] 
+    files = ["har_OpenML", "pendigits_OpenML", "eye_movements_OpenML"]
+    # files = ["letter_OpenML", "BNG(autos_COMMA_nominal_COMMA_1000000)_OpenML", "BNG(lymph_COMMA_nominal_COMMA_1000000)_OpenML"] 
     for filename in files:
         df = pd.read_csv(folderpath+filename+".csv")
         y=df["class"].to_numpy()
@@ -53,12 +53,12 @@ def AvgTime(algo, mode):
         
         print("***", filename, time_)
         
-AvgTime("AP", "SS")
-AvgTime("AP", "Default")
+# AvgTime("AP", "SS")
+# AvgTime("AP", "Default")
 
 
-# AvgTime("DBSCAN", "SS")
-# AvgTime("DBSCAN", "Default")
+AvgTime("DBSCAN", "SS")
+AvgTime("DBSCAN", "Default")
 
 
 # AvgTime("HAC", "SS")
