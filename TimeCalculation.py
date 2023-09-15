a72cb78d0f960c3080c3e06d7cb4d93158690323
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-09-15 17:59:34 -0400

    Rerun SC time calc with n clustering and 0.01 tol

--- TimeCalculation.py
+++ TimeCalculation.py
@@ -23,8 +23,8 @@ def TimeCalc(algo, mode, system):
         if os.path.isdir("Stats/Time/" + algo + "/") == 0:    
             os.mkdir("Stats/Time/" + algo + "/")
         f=open("Stats/Time/" + algo + "/"+ system + ".csv", "w")
-        f.write('Filename,Row,Columm,Estimated_Time,100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000\n')
-        # f.write('Filename,Row,Columm,Estimated_Time,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000\n')
+        # f.write('Filename,Row,Columm,Estimated_Time,100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000\n')
+        f.write('Filename,Row,Columm,Estimated_Time,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,\n')
         f.close()
     else:
         done_files = pd.read_csv("Stats/Time/" + algo + "/"+ system + ".csv")
@@ -53,9 +53,11 @@ def TimeCalc(algo, mode, system):
 def runfile(file, filename, algo, mode, system):
     print(filename)
     df = pd.read_csv(file)
+    gt = df["class"].to_numpy()
     df = df.drop(columns=["class"])
     row = df.shape[0]
     col = df.shape[1]
+    
     
     if row > 110000:
         return
@@ -63,19 +65,21 @@ def runfile(file, filename, algo, mode, system):
     #     print("Row:", row)
     #     return
     
-    # rows = [100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000]
-    rows = [100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000]
+    rows = [1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000]
+    # rows = [100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000]
     # rows = [300,600,900,1200,1500,1800]
     times = []
     for r in rows:
         print(r, end=' - ')
         X = df[:r]
+        n_c = len(np.unique(gt[:r]))
+        
         
         t0 = time.time()
         if algo == "AP":
             clustering = AffinityPropagation().fit(X)
         elif algo == "SC":
-            clustering = SpectralClustering(eigen_solver="amg").fit(X)
+            clustering = SpectralClustering(n_clusters=n_c,eigen_tol=0.01).fit(X)
         elif algo == "DBSCAN":
             clustering = DBSCAN().fit(X)
         else:
@@ -84,15 +88,15 @@ def runfile(file, filename, algo, mode, system):
         time_ = time.time()-t0
         print(time_)
         times.append(time_)
-        if (r < 1000 and time_ > 10) or time_ > 200:
-            time_str = ",".join(str(x) for x in times)
+        # if (r < 1000 and time_ > 10) or time_ > 200:
+        #     time_str = ",".join(str(x) for x in times)
             
-            estimated_time = predict_row_time(rows[:len(times)], times, row)
+        #     estimated_time = predict_row_time(rows[:len(times)], times, row)
             
-            f=open("Stats/Time/" + algo + "/"+ system + ".csv", "a")
-            f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+'\n')
-            f.close()
-            return
+        #     f=open("Stats/Time/" + algo + "/"+ system + ".csv", "a")
+        #     f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+'\n')
+        #     f.close()
+        #     return
         
         
     estimated_time = predict_row_time(rows, times, row)
@@ -131,23 +135,38 @@ def linRegresCalculate(algo, mode, system):
     fl = ["nomao_OpenML", "OnlineNewsPopularity_OpenML", "2dplanes_OpenML", "mv_OpenML", "numerai28.6_OpenML", "Diabetes130US_OpenML", "BNG(vote)_OpenML", "BNG(2dplanes)_OpenML"]
     for index, row in times.iterrows():
         
-        data = row[4:18].dropna()
-        if row["Filename"] not in fl:
-            continue
+        data = row[4:].dropna()
+        # if row["Filename"] not in fl:
+        #     continue
         x = data.index.values.reshape(-1, 1) 
         x = np.array([list(map(float, item)) for item in x])
 
         y = data.values.reshape(-1, 1) 
         # y_s = y
-        d = 2
+        d = 1
 
         poly_features = PolynomialFeatures(degree=d)
         x_poly = poly_features.fit_transform(x)
-        
+    
         model = LinearRegression(fit_intercept=False)
         model.fit(x_poly, y)
         
         y_pred = model.predict(x_poly)
+        
+        if algo == "AP":
+            overestimation_indices = np.where(y_pred > y)[0]
+            overestimated_actual = y[overestimation_indices]
+            overestimated_predicted = y_pred[overestimation_indices]
+    
+    
+            mse = round(mean_squared_error(overestimated_actual, overestimated_predicted), 1)
+            
+            if mse > 50:        
+                y_s = y*0.92
+                model = LinearRegression(fit_intercept=False)
+                model.fit(x_poly, y_s)
+                
+                y_pred = model.predict(x_poly)
         
         # y_corrected = np.copy(y)
         
@@ -172,11 +191,13 @@ def linRegresCalculate(algo, mode, system):
         
         x_test_poly = poly_features.transform(np.array([[row["Row"]]]))
         prediction = model.predict(x_test_poly)[0][0]
-        print(row["Filename"], row["Row"], ":", round(prediction, 2))
+        print(round(prediction, 0), "\t", row["Filename"], row["Row"], ":")
     
+        times.at[index, "Estimated_Time"] = prediction
         
-
-
+    # times.to_csv("Stats/Time/" + algo + "/"+ system + "_Est.csv", index=False)
+    
+    
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -213,15 +234,15 @@ def NN(algo, mode, system):
     print(data.head())
     
 
-# algo = sys.argv[1]
-# mode = sys.argv[2]
-# system = sys.argv[3]
+algo = sys.argv[1]
+mode = sys.argv[2]
+system = sys.argv[3]
 
-# TimeCalc(algo, mode, system)
+TimeCalc(algo, mode, system)
 
-# TimeCalc("SC", "Default", "Louise")
+# TimeCalc("SC", "Default", "Louise_test")
 
-linRegresCalculate("AP", "Default", "Jimmy_")
+# linRegresCalculate("SC", "Default", "Jimmy_def")
 # DecisionTree("SC", "Default", "Jimmy")
 
 
