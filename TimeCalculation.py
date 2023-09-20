96fc4958a6085dbdaf5554c12c35c4ccd85f0aaa
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-09-20 03:23:48 -0400

    SC tol 0.0001

--- TimeCalculation.py
+++ TimeCalculation.py
@@ -26,8 +26,8 @@ def TimeCalc(algo, mode, system):
             os.mkdir("Stats/Time/" + algo + "/")
         f=open("Stats/Time/" + algo + "/"+ system + ".csv", "w")
         # f.write('Filename,Row,Columm,Estimated_Time,100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000\n')
-        # f.write('Filename,Row,Columm,Estimated_Time,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,\n')
-        f.write('Filename,Row,Columm,Time,ARI\n')
+        f.write('Filename,Row,Columm,Estimated_Time,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,Actual_Time\n')
+        # f.write('Filename,Row,Columm,Time,ARI\n')
 
         f.close()
     else:
@@ -40,18 +40,13 @@ def TimeCalc(algo, mode, system):
     FileList = pd.read_csv("MemoryStats/FileList.csv")
     FileList = FileList["Filename"].to_numpy()
     
-    support_files = ["eye_movements_OpenML", "gas-drift-different-concentrations_OpenML", "gas-drift_OpenML", "sylva_prior_OpenML", "fried_OpenML", "mnist_784_OpenML", "numerai28.6_OpenML", "Diabetes130US_OpenML"]
-    
     for file in master_files:
         filename = file.split("/")[-1]
         filename = filename[:-4]
-        # if filename not in FileList or filename == "2dplanes_OpenML":
-        if filename not in FileList:
+        if filename not in FileList or filename == "2dplanes_OpenML":
             continue
         if filename in done_files:
             print("Already done", filename)
-            continue
-        if filename not in support_files:
             continue
         runfile(file, filename, algo, mode, system)
         
@@ -74,10 +69,10 @@ def runfile(file, filename, algo, mode, system):
     #     print("Row:", row)
     #     return
     
-    rows = [1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000]
+    rows = [1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000, row]
     # rows = [100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000]
     # rows = [300,600,900,1200,1500,1800]
-    rows = [row]
+    # rows = [row]
     times = []
     # ari = -2
     for r in rows:
@@ -90,9 +85,9 @@ def runfile(file, filename, algo, mode, system):
         if algo == "AP":
             clustering = AffinityPropagation().fit(X)
         elif algo == "SC":
-            clustering = SpectralClustering(n_clusters=n_c).fit(X)
-            labels = clustering.labels_
-            ari = adjusted_rand_score(gt, labels)
+            clustering = SpectralClustering(n_clusters=n_c,eigen_tol=0.0001).fit(X)
+            # labels = clustering.labels_
+            # ari = adjusted_rand_score(gt, labels)
         elif algo == "DBSCAN":
             clustering = DBSCAN().fit(X)
         else:
@@ -112,12 +107,12 @@ def runfile(file, filename, algo, mode, system):
         #     return
         
         
-    # estimated_time = predict_row_time(rows, times, row)
+    estimated_time = predict_row_time(rows, times, row)
     
     time_str = ",".join(str(x) for x in times)
     f=open("Stats/Time/" + algo + "/"+ system + ".csv", "a")
-    # f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+'\n')
-    f.write(filename+','+str(row)+','+str(col)+','+time_str+','+str(ari)+'\n')
+    f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+'\n')
+    # f.write(filename+','+str(row)+','+str(col)+','+time_str+','+str(ari)+'\n')
     f.close()
         
         
