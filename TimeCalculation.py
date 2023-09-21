4dda8d3c29998f83b74a2abfa9e7634da506def3
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-09-20 20:12:11 -0400

    SC tol null

--- TimeCalculation.py
+++ TimeCalculation.py
@@ -26,7 +26,7 @@ def TimeCalc(algo, mode, system):
             os.mkdir("Stats/Time/" + algo + "/")
         f=open("Stats/Time/" + algo + "/"+ system + ".csv", "w")
         # f.write('Filename,Row,Columm,Estimated_Time,100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000\n')
-        f.write('Filename,Row,Columm,Estimated_Time,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,Actual_Time,ARI\n')
+        f.write('Filename,Row,Columm,Estimated_Time,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000\n')
         # f.write('Filename,Row,Columm,Time,ARI\n')
 
         f.close()
@@ -69,12 +69,12 @@ def runfile(file, filename, algo, mode, system):
     #     print("Row:", row)
     #     return
     
-    rows = [1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000, row]
+    rows = [1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000]
     # rows = [100,200,300,400,500,600,700,800,900,1000,2000,3000,6000,9000,12000,15000,20000]
     # rows = [300,600,900,1200,1500,1800]
     # rows = [row]
     times = []
-    ari = -2
+    # ari = -2
     for r in rows:
         print(r, end=' - ')
         X = df[:r]
@@ -85,7 +85,7 @@ def runfile(file, filename, algo, mode, system):
         if algo == "AP":
             clustering = AffinityPropagation().fit(X)
         elif algo == "SC":
-            clustering = SpectralClustering(n_clusters=n_c,eigen_tol=0.1).fit(X)
+            clustering = SpectralClustering(n_clusters=n_c).fit(X)
             if r == row:
                 labels = clustering.labels_
                 ari = adjusted_rand_score(gt, labels)
@@ -112,7 +112,7 @@ def runfile(file, filename, algo, mode, system):
     
     time_str = ",".join(str(x) for x in times)
     f=open("Stats/Time/" + algo + "/"+ system + ".csv", "a")
-    f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+','+str(ari)+'\n')
+    f.write(filename+','+str(row)+','+str(col)+','+str(estimated_time)+','+time_str+'\n')
     # f.write(filename+','+str(row)+','+str(col)+','+time_str+','+str(ari)+'\n')
     f.close()
         
