4e5698f4ea3b773ebebb26357a381b2c761006e4
Muyeed Ahmed <muyeedahmed@Muyeeds-MacBook-Air.local>
2024-02-24 04:04:47 -0500

    Add prints back prints

--- PAU/PAU_Clustering.py
+++ PAU/PAU_Clustering.py
@@ -291,7 +291,7 @@ class PAU_Clustering:
                 batch_index += 1
             for t in threads:
                 t.join()
-        # print("Before Merge")
+        print("Before Merge")
 
         if self.mergeStyle == "Distance":
             self.constantKMerge()
@@ -393,9 +393,9 @@ class PAU_Clustering:
         csv_files = glob.glob('Output/Temp/*.{}'.format('csv'))
         csv_files.sort()
         while len(csv_files) > 1:
-            # print("len(csv_files)", len(csv_files))
+            print("len(csv_files)", len(csv_files))
             for i in range(0, len(csv_files)-1, 2):
-                # print("Start")
+                print("Start")
                 file1 = csv_files[i]
                 file2 = csv_files[i+1]
                 df1 = pd.read_csv(file1)
@@ -419,7 +419,7 @@ class PAU_Clustering:
                 
                 global_centers = []
                 global_centers_frequency = []
-                # print("Start global_centers_frequency.append")
+                print("Start global_centers_frequency.append")
 
                 global_centers_count = len(unique_labels1)
                 for i in unique_labels1:
@@ -427,7 +427,7 @@ class PAU_Clustering:
                     global_centers_frequency.append(len(X_1[labels1 == i]))
                 
                 df2["ll"] = -2
-                # print("Start nearest_cluster")
+                print("Start nearest_cluster")
                 
                 for i in unique_labels2:
                     c = X_2[labels2 == i].mean(axis=0)
@@ -436,7 +436,7 @@ class PAU_Clustering:
                     df2.loc[df2['l'] == i, 'll'] = nearest_cluster
                 df2 = df2.drop("l", axis=1)
                 df2 = df2.rename(columns={'ll': 'l'})
-                # print("Done nearest_cluster")
+                print("Done nearest_cluster")
         
                 df = pd.concat([df1, df2])
                 df.to_csv(file2, index=False)
@@ -666,6 +666,8 @@ class PAU_Clustering:
         return ari
     
     def run(self):
+        if self.X.shape[1] > 50:
+            return -2, -1
         t0 = time.time()
         if self.batch_count == 0:
             if self.X.shape[0] < 10000:    
@@ -675,9 +677,9 @@ class PAU_Clustering:
             else:
                 self.batch_count = int(self.X.shape[0]/100000)*100
         self.subSample()
-        # print("subSample")
+        print("subSample")
         self.rerun()
-        # print("rerun")
+        print("rerun")
 
         t1 = time.time()
         ari_ss = self.AUL_ARI()
