a119e96eea3809ab1a61b92e159bab41de4d7234
Muyeed Ahmed <muyeedahmed@Muyeeds-MacBook-Air.local>
2024-02-25 22:20:00 -0500

    Time dist plot

--- AblationTest.py
+++ AblationTest.py
@@ -36,7 +36,9 @@ def ModuleWiseTimeDist(algo):
         Series[f"{ind}"] = merged_df.loc[ind].to_numpy()
 
     fig, ax = timeDist(Series, ["3.1 Data Partitioning", "3.2 Tuning Parameters for HAPV", "3.3 Generating Labels", "3.4 Merging Labels"])
+    ax.set_title(algo)
     fig.savefig("Figures/TimeDist/"+algo+".pdf")
+
 
 
 def timeDist(results, category_names):
@@ -45,28 +47,52 @@ def timeDist(results, category_names):
     data_cum = data.cumsum(axis=1)
     category_colors = ['gold', 'tomato', 'limegreen', 'dodgerblue']
 
-
-    fig, ax = plt.subplots(figsize=(7, 5))
-    ax.invert_yaxis()
-    ax.xaxis.set_visible(False)
-    ax.set_xlim(0, np.sum(data, axis=1).max())
+    fig, ax = plt.subplots(figsize=(5, 10))  # Adjust figsize to fit the rotated plot
+    ax.xaxis.set_visible(True)
+    ax.yaxis.set_visible(False)  # Make y-axis visible
+    ax.set_ylim(0, np.sum(data, axis=1).max())  # Set y-axis limits
 
     for i, (colname, color) in enumerate(zip(category_names, category_colors)):
         widths = data[:, i]
         starts = data_cum[:, i] - widths
-        rects = ax.barh(labels, widths, left=starts, height=0.5,
-                        label=colname, color=color)
-        # r, g, b, _ = color
-        # text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
-        # ax.bar_label(rects, label_type='center', color=text_color)
-    # ax.legend(ncols=len(category_names), bbox_to_anchor=(0, 1),
-    #           loc='lower left', fontsize='small')
-    legend = ax.legend(ncol=2, bbox_to_anchor=(0, 1),
-                       loc='lower left', fontsize='small', title='')
-    # legend.get_title().set_alignment('left')  # Set legend title alignment to left
+        rects = ax.bar(labels, widths, bottom=starts, width=0.5, 
+                       label=colname, color=color, orientation='vertical')  # Rotate bars vertically
 
-
+    legend = ax.legend(ncol=2, bbox_to_anchor=(0.5, 1.15),  # Move legend to top and center
+                       loc='upper center', fontsize=10, title='', frameon=False)
+    ax.xaxis.set_tick_params(rotation=90)  # Rotate x-axis labels by 90 degrees
+    
     return fig, ax
+
+
+# def timeDist(results, category_names):
+#     labels = list(results.keys())
+#     data = np.array(list(results.values()))
+#     data_cum = data.cumsum(axis=1)
+#     category_colors = ['gold', 'tomato', 'limegreen', 'dodgerblue']
+
+
+#     fig, ax = plt.subplots(figsize=(10, 5))
+#     ax.invert_yaxis()
+#     ax.xaxis.set_visible(False)
+#     ax.set_xlim(0, np.sum(data, axis=1).max())
+
+#     for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+#         widths = data[:, i]
+#         starts = data_cum[:, i] - widths
+#         rects = ax.barh(labels, widths, left=starts, height=0.5,
+#                         label=colname, color=color)
+#         # r, g, b, _ = color
+#         # text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+#         # ax.bar_label(rects, label_type='center', color=text_color)
+#     # ax.legend(ncols=len(category_names), bbox_to_anchor=(0, 1),
+#     #           loc='lower left', fontsize='small')
+#     legend = ax.legend(ncol=2, bbox_to_anchor=(0, 1),
+#                        loc='lower left', fontsize=10, title='')
+#     # legend.get_title().set_alignment('left')  # Set legend title alignment to left
+
+
+#     return fig, ax
 
     
 def BoxPlotMode():
